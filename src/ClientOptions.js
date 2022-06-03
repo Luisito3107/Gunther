304c1081f1d5e873269a14187058a393208747d7
Luis Lezama <luislez@aol.com>
2022-06-03 01:48:11 -0500

    Updated package.json

--- src/ClientOptions.js
+++ src/ClientOptions.js
@@ -10,9 +10,3 @@ const {
 module.exports = {
     intents: [Guilds, GuildVoiceStates, GuildMessages, GuildMessageReactions, MessageContent]
 }
-
-/*const {Intents} = require("discord.js");
-
-module.exports = {
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
-}*/
