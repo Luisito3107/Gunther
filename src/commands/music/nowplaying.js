f7fbfc90acb610e165e92a67ef444f50d4ab3730
Luis Lezama <luislez@aol.com>
2022-06-06 00:46:39 -0500

    Fixed nowplaying command

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -13,36 +13,40 @@ module.exports = {
             player.get("nowplayingMSG").delete().catch(_ => void 0);
         } 
 
-        let musicLength = player.queue.current.isStream ? null : ((!player.queue.current || !player.queue.current.duration || isNaN(player.queue.current.duration)) ? null : player.queue.current.duration),
-            nowTime = (!player.position || isNaN(player.position)) ? null : player.position;
-
-        const embed = (p, l, n) => new EmbedBuilder()
-            .setAuthor({name: "Now playing", iconURL: client.assetsURL_icons+"/vinyl.gif"})
-            .setColor(/*client.EMBED_COLOR()*/ "#50618e")
-
-            .setTitle(`${p.queue.current.title}`)
-            .setURL(p.queue.current.uri)
-            .setThumbnail(p.queue.current.thumbnail || null)
-            .setDescription(
-                `**Requested by: ** ${p.queue.current.requester}\n\n`+
-                (p.queue.current.isStream ? '🔴 LIVE' :
-                    `\`${client.formatDuration(p.position)}\` `+
-                    splitBar(l ? Number(l) : 1, n ? Number(n) : 2, 16, '━', '🔵')[0]+
-                    ` \`${client.formatDuration(p.queue.current.duration)}\``
-                )
-            )
-            if (!p.queue.current.isStream) embed.setFooter({text: `${client.formatDuration(l - n) + ' left'}`})
-
         await ctx.deferReply();
-        ctx.editReply({embeds: [embed(player, musicLength, nowTime)]}).then(m => player.set("nowplayingMSG", m));
-
-        const interval = setInterval(() => {
-            player = client.player.players.get(ctx.guildId);
+        try {
             let musicLength = player.queue.current.isStream ? null : ((!player.queue.current || !player.queue.current.duration || isNaN(player.queue.current.duration)) ? null : player.queue.current.duration),
                 nowTime = (!player.position || isNaN(player.position)) ? null : player.position;
 
-            return player ? player.get("nowplayingMSG") ? player.get("nowplayingMSG").edit({embeds: [embed(player, musicLength, nowTime)]}).catch(_ => clearInterval(interval)) : void 0 : clearInterval(interval);
-        }, 5000);
-        player.set("nowplaying", interval);
+            const embed = (p, l, n) => new EmbedBuilder()
+                .setAuthor({name: "Now playing", iconURL: client.assetsURL_icons+"/vinyl.gif"})
+                .setColor(/*client.EMBED_COLOR()*/ "#50618e")
+
+                .setTitle(`${p.queue.current.title}`)
+                .setURL(p.queue.current.uri)
+                .setThumbnail(p.queue.current.thumbnail || null)
+                .setDescription(
+                    `**Requested by: ** ${p.queue.current.requester}\n\n`+
+                    (p.queue.current.isStream ? '🔴 LIVE' :
+                        `\`${client.formatDuration(p.position)}\` `+
+                        splitBar(l ? Number(l) : 1, n ? Number(n) : 2, 16, '━', '🔵')[0]+
+                        ` \`${client.formatDuration(p.queue.current.duration)}\``
+                    )
+                )
+                .setFooter({text: `${(!p.queue.current.isStream ? client.formatDuration(l - n) + ' left' : '&nbsp;')}`})
+
+            ctx.editReply({embeds: [embed(player, musicLength, nowTime)]}).then(m => player.set("nowplayingMSG", m));
+
+            const interval = setInterval(() => {
+                player = client.player.players.get(ctx.guildId);
+                let musicLength = player.queue.current.isStream ? null : ((!player.queue.current || !player.queue.current.duration || isNaN(player.queue.current.duration)) ? null : player.queue.current.duration),
+                    nowTime = (!player.position || isNaN(player.position)) ? null : player.position;
+
+                return player ? player.get("nowplayingMSG") ? player.get("nowplayingMSG").edit({embeds: [embed(player, musicLength, nowTime)]}).catch(_ => clearInterval(interval)) : void 0 : clearInterval(interval);
+            }, 5000);
+            player.set("nowplaying", interval);
+        } catch (e) {
+            ctx.editReply({embeds: [this.baseEmbed("💣 | Oops, an error occurred! Please try again in a few minutes.\n" + `\`\`\`${e ? e : 'No error was provided'}\`\`\``)]});
+        }
     }
 }
